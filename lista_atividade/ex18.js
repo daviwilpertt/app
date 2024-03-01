36194d826774d9d703859479b1e2c398ec468e01
Davi Wilpert <daviwilpertt@proton.me>
2024-02-29 21:52:20 -0300

    jh

--- lista_atividade/ex18.js
+++ lista_atividade/ex18.js
@@ -36,10 +36,21 @@ const usuarios = [
       temAnimalEstimacao: false,
     },
   ];
-  function nome_completo() {
-    for (let i = 0; i < usuarios.length; i++) {
-        let nome_sobrenome = `${usuarios[i].nome} ${usuarios[i].sobrenome} ` 
-        console.log(nome_sobrenome)
-    }
-}
-nome_completo()
+//   function nome_completo() {
+//     for (let i = 0; i < usuarios.length; i++) {
+//         let nome_sobrenome = `${usuarios[i].nome} ${usuarios[i].sobrenome} `
+//         console.log(nome_sobrenome)
+//     }
+// }
+// nome_completo()
+
+let so_nome = usuarios.map((item) => {
+  return {
+    nome: item.nome,
+    sobrenome: item.sobrenome,
+    nome_completo: `${item.nome} ${item.sobrenome}`,
+    idade: item.idade,
+    maior_idade: item.idade >= 18
+  }
+})
+console.log(so_nome)
