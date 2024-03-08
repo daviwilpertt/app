6f32701657f982fa7bebe76825070df9d85f8879
Davi Wilpert <daviwilpertt@proton.me>
2024-03-07 21:51:45 -0300

    fd

---
+++ projeto0/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>abre o teto </h1>
+      </header>
+
+      <aside>da panamera aiai</aside>
+
+      <footer>
+        pooooooooooooo
+      </footer>
+      <p>piparaparapo</p>
+    </>
+  )
+}
+
+export default App
