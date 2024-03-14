4674593c7385c84840517d5d628ccb1e5deb24c1
Davi Wilpert <daviwilpertt@proton.me>
2024-03-13 21:58:40 -0300

    sdfd

--- projeto0/src/componentes/Rodape/Rodape.jsx
+++ projeto0/src/componentes/Rodape/Rodape.jsx
@@ -1,10 +1,12 @@
 import './Rodape.css'
 
 const Rodape = () => {
+    const ano_atual = new Date().getFullYear();
+
     return <>
         <footer>
             <p>
-                peida nao xerequinha
+                Copyright @ {ano_atual} - Todos os direitos reservados - Davi de Souza
             </p>
         </footer>
     </>
