2e0a37fb7c93d660510b46c4f81fa7f290beb91f
Davi Wilpert <daviwilpertt@proton.me>
2024-03-21 21:46:35 -0300

    fdf

--- exercicios-frontend-senai/src/componentes/NumerosImpares/NumerosImpares.jsx
+++ exercicios-frontend-senai/src/componentes/NumerosImpares/NumerosImpares.jsx
@@ -3,12 +3,15 @@ import './NumerosImpares.css'
 const NumerosImpares = (props) => {
 
     let numeros = props.numeros
-    if (numeros % 2 === 1) {
-        return <div className='numerosImpares'>
-            <ul>
-                <li>{numeros}</li>
-            </ul>
-        </div>
-    }
+
+    return <div className='numerosImpares'>
+        <ul>
+            {numeros.filter(numero => numero % 2 === 1).map((numero, index) => (
+                <li key={index}>{numero}</li>
+                ))}
+
+        </ul>
+    </div>
 }
+
 export default NumerosImpares
--- exercicios-frontend-senai/src/componentes/NumerosPares/NumerosPares.jsx
+++ exercicios-frontend-senai/src/componentes/NumerosPares/NumerosPares.jsx
@@ -3,15 +3,15 @@ import './NumerosPares.css'
 const NumerosPares = (props) => {
     
     let numeros = props.numeros
-    if (numeros % 2 === 0) {
+
         return <div className='numerosPares'>
             <ul>
-                {numeros.map((numero, index) => (
+                {numeros.filter(numero => numero % 2 === 0).map((numero, index) => (
                     <li key={index}>{numero}</li>
                 ))}
             </ul>
         </div>
         }
-}
+
 
 export default NumerosPares
