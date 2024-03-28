e5bfffcd3ba705c3b96c69e827038d0f27213f5c
Davi Wilpert <daviwilpertt@proton.me>
2024-03-28 20:31:39 -0300

    fd

--- exercicios-frontend-ii-senai/src/App.jsx
+++ exercicios-frontend-ii-senai/src/App.jsx
@@ -1,9 +1,26 @@
+import BotaoContador from "./componentes/BotaoContador/BotaoContador";
+import Contador from "./componentes/Contador/Contador";
+
 function App() {
   return (
     <>
       <span>Exercício 1</span>
-
+      <br />
+      <BotaoContador />
       <hr />
+
+      <span>Exercício 2</span>
+      <br />
+      <Contador />
+      <hr />
+
+      <span>Exercício 3</span>
+      <br />
+      
+      <hr />
+
+
+
     </>
   );
 }
