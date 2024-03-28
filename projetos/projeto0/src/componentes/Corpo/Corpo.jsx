d1abe5fe103899d441299209f6d07bb1fa9e4a5f
Davi Wilpert <daviwilpertt@proton.me>
2024-03-27 21:51:28 -0300

    dsd

--- projetos/projeto0/src/componentes/Corpo/Corpo.jsx
+++ projetos/projeto0/src/componentes/Corpo/Corpo.jsx
@@ -2,18 +2,24 @@ import './Corpo.css'
 import Botao_custom from '../Botao_custom/Botao_custom';
 import ListaProdutos from '../ListaProdutos/ListaProdutos';
 import BotaoContador from '../BotaoContador/BotaoContador';
+import { useState } from 'react';
+import Formulario from '../Formulario/Formulario';
 const Corpo = () => {
+  const [exemplo, setExemplo] = useState('')
+  const mudar = () => {
+    setExemplo()
+  }
   return (
     <main>
-      <Botao_custom tipo='primario'onClick={() => { alert('Salvo com sucesso!')}}
-      >
-        Salvar
-      </Botao_custom>
-      <Botao_custom tipo='secundario'>Git</Botao_custom>
-      <Botao_custom>Cancelar</Botao_custom>
-      <BotaoContador />
+      <Botao_custom tipo='secundario' onClick={() => setExemplo('ListaProdutos')}>Lista Produtos</Botao_custom>
+      <Botao_custom onClick={() => setExemplo('BotaoContador')}>Botão Contador</Botao_custom>
+      <Botao_custom onClick=
 
-      <ListaProdutos />
+      {exemplo === 'ListaProdutos' && <ListaProdutos />}
+      {exemplo === 'BotaoContador' && <BotaoContador />}
+
+      <Formulario />
+
     </main>
   );
 };
