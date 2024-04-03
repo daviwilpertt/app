32e18052133fd48c03eb5a70dc0c8df9f07c8484
Davi Wilpert <daviwilpertt@proton.me>
2024-04-03 19:25:33 -0300

    fdf

--- exercicios-frontend-ii-senai/src/App.jsx
+++ exercicios-frontend-ii-senai/src/App.jsx
@@ -1,25 +1,41 @@
 import BotaoContador from "./componentes/BotaoContador/BotaoContador";
 import Contador from "./componentes/Contador/Contador";
+import Diminuir from "./componentes/Diminuir/Diminuir";
+import MostrarTela from "./componentes/MostrarTela/MostrarTela";
+import Somar from "./componentes/Somar/Somar";
 
 function App() {
   return (
     <>
-      <span>Exercício 1</span>
-      <br />
-      <BotaoContador />
-      <hr />
+      <span>
+        <p>Exercício 1</p>
+        <BotaoContador />
+        <hr />
+      </span>
 
-      <span>Exercício 2</span>
-      <br />
-      <Contador />
-      <hr />
+      <span>
+        <p>Exercício 2</p>
+        <Contador />
+        <hr />
+      </span>
 
-      <span>Exercício 3</span>
-      <br />
-      
-      <hr />
+      <span>
+        <p>Exercício 3</p>
+        <MostrarTela />
+        <hr />
+      </span>
 
+      <span>
+        <p>Exercício 4</p>
+        <Somar />
+        <hr />
+      </span>
 
+      <span>
+        <p>Exercício 5</p>
+        <Diminuir />
+        <hr />
+      </span>
 
     </>
   );
--- exercicios-frontend-ii-senai/src/componentes/MostrarTela/MostrarTela.jsx
+++ exercicios-frontend-ii-senai/src/componentes/MostrarTela/MostrarTela.jsx
@@ -3,7 +3,16 @@ import './MostrarTela.css'
 
 const MostrarTela = () => {
     const [tela, setTela] = React.useState('') 
+    const conteudo = (texto) => {
+        setTela(texto)
+    }
 
+    return <>
+        <button onClick={() => conteudo('conteúdo tela 1')}>Tela I</button>
+        <button onClick={() => conteudo('conteúdo tela 2')}>Tela II</button>
+        <button onClick={() => conteudo('conteúdo tela 3')}>Tela III</button>
+        <p>{tela}</p>
+    </>
 
 }
 
